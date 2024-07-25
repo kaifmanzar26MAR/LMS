2f3ba76a2443be7f46aac5a89911e083c4beb635
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-07-26 02:17:32 +0530

    updated the top padding in the layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <SideBar />
       </div>
-      <main className="md:pl-56 h-full">{children}</main>
+      <main className="md:pl-56 h-full pt-[80px]">{children}</main>
     </div>
   );
 };
